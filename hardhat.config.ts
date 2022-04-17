6ae1cfd74f5f9c3734b01af11d2b86c4b53c5dac
Contreux <52302850+Contreux@users.noreply.github.com>
2022-04-17 15:18:04 +1000

    Add support for testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,22 +1,3 @@
-// require("@nomiclabs/hardhat-waffle");
-// module.exports = {
-//   solidity: {
-//     version: "0.7.5",
-//     settings: {
-//       optimizer: {
-//         enabled: true,
-//         runs: 200
-//       }
-//     }
-//   },
-//   defaultNetwork: "mainnet",
-//   networks:{
-//     mainnet: {
-//       url: "https://api.avax.network/ext/bc/C/rpc",
-//       accounts: ["d6314c428ef4600d9f501b114e44241d6fd37df704dd3a184557a111bc96db1f"],
-//     }
-//   },
-// };
 
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
@@ -35,10 +16,14 @@ export default {
       }
     }
   },
-  // defaultNetwork: "mainnet",
-  // networks:{
-  //   mainnet: {
-  //     url: "https://api.avax.network/ext/bc/C/rpc",
-  //   }
-  // },
+  defaultNetwork: "mainnet",
+  networks:{
+    mainnet: {
+      url: "https://api.avax.network/ext/bc/C/rpc",
+    },
+    testnet: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+    }
+  },
 }
+
